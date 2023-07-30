e13a7123b0d58ee9fa656362fb9a15f57e64e028
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-07-30 12:37:27 -0400

    refactor: fix nextPage token issue and remove verification of duplicated file

--- main.py
+++ main.py
@@ -5,20 +5,15 @@ from config.sqlachemy import SessionLocal
 from models.console import Console
 from models.game import Game
 from sqlalchemy.exc import IntegrityError
+import os
 
 def insert_file_data(file_name: str, file_url: str, console: Console, session: Session):
     try:
-        # Check if the file with the same name already exists for the console
-        existing_file = session.query(Game).filter_by(name=file_name, console_id=console.id).first()
-        if existing_file:
-            print(f"File '{file_name}' already exists for console '{console.name}'. Skipping...")
-            return
-
-        # Add the file to the database
-        print(f"Inserting file '{file_name}'...")
-        file = Game(name=file_name, url=file_url, console_id=console.id)
+          # Remove the file extension from the file name
+        game_name = os.path.splitext(file_name)[0]
+        print(f"Inserting file '{game_name}'...")
+        file = Game(name=game_name, url=file_url, console_id=console.id)
         session.add(file)
-        session.commit()
     except IntegrityError as e:
         # Handle any integrity error (e.g., duplicate key) that might occur during commit
         session.rollback()
@@ -28,25 +23,35 @@ def insert_file_data(file_name: str, file_url: str, console: Console, session: S
         session.rollback()
         print(f"Error inserting file '{file_name}': {str(e)}")
 
-
 def main():
     drive_service = get_drive_service()
     session = SessionLocal()
 
     for game_config in games_config:
         console = session.query(Console).filter_by(name=game_config['name']).first()
-        response = drive_service.files().list(q=f"'{game_config['id']}' in parents",
-                                              fields="files(id, name, webViewLink)",
-                                              pageSize=200).execute()
-        files = response.get('files', [])
-        for file in files:
-            file_name = file.get('name', '')
-            file_url = file.get('webViewLink', '')
-            print(f"Found file: {file_name} ({file_url})")
-            insert_file_data(file_name, file_url, console, session)
+        page_token = None
+        while True:
+            query_params = {'q': f"'{game_config['id']}' in parents",
+                            'fields': "files(id, name, webViewLink), nextPageToken",
+                            'pageSize': 500}
+            if page_token:
+                query_params['pageToken'] = page_token
 
+            response = drive_service.files().list(**query_params).execute()
+            files = response.get('files', [])
+            for file in files:
+                file_name = file.get('name', '')
+                file_url = file.get('webViewLink', '')
+                print(f"Found file: {file_name} ({file_url})")
+                insert_file_data(file_name, file_url, console, session)
+            print("Committing changes...")
+            session.commit()
+            page_token = response.get('nextPageToken')
+            if not page_token:
+                break
+
+            
     session.close()
 
 if __name__ == "__main__":
     main()
-
